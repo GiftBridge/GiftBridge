a6c6aadbdd7a60583bea0e3c8a9ddd90cbfc4dfb
GiftBridge <idgafvfx@gmail.com>
2025-07-30 20:36:57 +0530

    Add global styles and update homepage

--- layout.js
+++ layout.js
@@ -1,3 +1,5 @@
+import './globals.css';
+
 export const metadata = {
   title: 'GiftBridge',
   description: 'Grant wishes for $3',
--- page.js
+++ page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to GiftBridge</h1>
+      <h1>GiftBridge</h1>
+      <p>Post your wish for $3. Let the kindness find you.</p>
+      <button>Post a Wish</button>
     </main>
   );
 }
